c4d325db3c443b85bfb96aa5059e21cb8fc57ff4
= <=>
2021-05-06 22:45:11 +0200

    Shipping Website

---
+++ js/plugin.js
@@ -0,0 +1,6 @@
+$('html').css("overflow-x","hidden");
+$('body').css("overflow","visible");
+$('body').css("overflow-x","hidden");
+/* Show Section Track */
+
+$(".track-sm").css("overflow","auto");
